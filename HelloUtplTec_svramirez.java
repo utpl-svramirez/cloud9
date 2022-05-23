1b2aad37bc72f8c764fef33e4b5e186b7ef405e4
utpl-svramirez <svramirez@utpl.edu.ec>
2022-05-23 17:57:13 +0000

    nuevo archivo añadido HelloUtplTec_svramirez.java en Cloud9

---
+++ HelloUtplTec_svramirez.java
@@ -0,0 +1,10 @@
+/**
+ * Class Untitled
+ */
+public class HelloUtplTec_svramirez {
+	
+	public static void main(String[] args) {
+    	System.out.println("Hello World UTPL TEC from AWS Cloud9! - svramirez");
+    	
+	}
+}
